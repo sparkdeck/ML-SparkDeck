289c7d821fa9728f1b8190aa1c0df52d5f0692c5
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-02-28 14:29:39 +0000

    updated chinese translation

--- fig_code/__init__.py
+++ fig_code/__init__.py
@@ -1,4 +1,6 @@
 from .data import *
 from .figures import *
 
-from .sgd_separator impor
+from .sgd_separator import plot_sgd_separator
+from .linear_regression import plot_linear_regression
+from .
