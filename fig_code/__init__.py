4b91175c72b0538756440b241599ce5209219f1c
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-03-02 07:48:07 +0000

    Exercise 2

--- fig_code/__init__.py
+++ fig_code/__init__.py
@@ -3,4 +3,5 @@ from .figures import *
 
 from .sgd_separator import plot_sgd_separator
 from .linear_regression import plot_linear_regression
-from .
+from .kmeans import plot_kmeans
+from .helpers import plot_iris_knn
