309c29a963f6602446f175f919517c7f1d4e316f
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-03-13 07:20:26 +0000

    fixing travis.yml

--- fig_code/helpers.py
+++ fig_code/helpers.py
@@ -2,4 +2,7 @@
 Small helpers for code that is not shown in the notebooks
 """
 
-from sklearn import neighbors,
+from sklearn import neighbors, datasets, linear_model
+import pylab as pl
+import numpy as np
+from matplotlib.colors import L
