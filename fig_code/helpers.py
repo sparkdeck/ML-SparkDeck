74b03e16a4e61bee59534ec9e388b5e82c7ecf46
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-03-13 14:49:17 +0000

    updated some stuff

--- fig_code/helpers.py
+++ fig_code/helpers.py
@@ -5,4 +5,7 @@ Small helpers for code that is not shown in the notebooks
 from sklearn import neighbors, datasets, linear_model
 import pylab as pl
 import numpy as np
-from matplotlib.colors import L
+from matplotlib.colors import ListedColormap
+
+# Create color maps for 3-class classification problem, as with iris
+cmap_light = Lis
