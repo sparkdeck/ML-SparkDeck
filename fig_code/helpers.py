3bb247f079c2df8adee82c2d80eb7eb64bed7bca
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-03-29 10:20:47 +0000

    Bump ini from 1.3.5 to 1.3.7

--- fig_code/helpers.py
+++ fig_code/helpers.py
@@ -52,4 +52,6 @@ def plot_polynomial_regression():
 
     X = np.array([x**i for i in range(5)]).T
     X_test = np.array([x_test**i for i in range(5)]).T
-    regr 
+    regr = linear_model.LinearRegression()
+    regr.fit(X, y)
+    pl.plot(x_test, regr.predict
