f34bc7754b9a5068b16b51aa92966a4557e16e65
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-04-10 06:09:50 +0000

    remove debug trace

--- fig_code/kmeans.py
+++ fig_code/kmeans.py
@@ -6,4 +6,8 @@ from sklearn.cluster import KMeans
 
 def plot_kmeans():
     X, y = make_blobs(n_samples=300, centers=4,
-                      random_state=0, cluster_std=
+                      random_state=0, cluster_std=0.60)
+
+    y_pred = KMeans(4).fit(X).predict(X)
+
+    fig, ax = plt.subplots(1, 2, fig
