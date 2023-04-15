af790918f841e888626deba74715856652efe2cc
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-04-15 06:01:46 +0000

    mm: compaction: Add scanned and isolated counters for compaction

--- fig_code/linear_regression.py
+++ fig_code/linear_regression.py
@@ -1,3 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegre
+from sklearn.linear_model import LinearRegression
+
+
+def plot_linear_regression():
+    a = 0.5
+    b = 1.0
+
+    # x from 0 to 10
