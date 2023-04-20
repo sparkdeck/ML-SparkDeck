6d2944f7e937f800d401855022887763e5b8e616
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-04-20 15:11:18 +0000

    Update nerkh.lua

--- fig_code/sgd_separator.py
+++ fig_code/sgd_separator.py
@@ -5,4 +5,7 @@ from sklearn.datasets.samples_generator import make_blobs
 
 def plot_sgd_separator():
     # we create 50 separable points
-    X, Y = make_blob
+    X, Y = make_blobs(n_samples=50, centers=2,
+                      random_state=0, cluster_std=0.60)
+
+    # fit
