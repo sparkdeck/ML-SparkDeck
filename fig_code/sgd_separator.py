5d86d047afdbc061d5cd025a9334c6c8e51ec507
sparkdeck <164767928+sparkdeck@users.noreply.github.com>
2023-04-20 09:47:08 +0000

    boolean

--- fig_code/sgd_separator.py
+++ fig_code/sgd_separator.py
@@ -1,4 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import SGDClassifier
-from sklearn.datasets.samples_generator import
+from sklearn.datasets.samples_generator import make_blobs
+
+def plot_sgd_separator():
+    # we create 50 separable points
+    X, Y = make_blob
